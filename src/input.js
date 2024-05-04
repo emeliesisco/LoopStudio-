3e22434fddc2b725e2e2d9de3bca25f7180aa651
Sisco <EmelieSisco9@gmail.com>
2024-05-04 14:48:58 +0100

    initial commit

---
+++ src/input.js
@@ -0,0 +1,20 @@
+// @tailwind base;
+// @tailwind components;
+// @tailwind utilities;
+
+
+tailwind.config = {
+  theme: {
+    screens:{
+      sm:'480px',
+      md:'768px',
+      lg:'976px',
+      xl:'1440px'
+    },
+    extend: {
+      },
+      letterSpacing:{
+        widest: '.3em',
+      }
+    }
+  }
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./dist/*.{index,js}*'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
